01c628d531671975c371f5bc279ddc7a551f6e67
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-15 19:37:57 +0100

    Index: Replace JSX with semantic components

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,18 +1,19 @@
 import Link from 'next/link'
+import { Heading, Main, Paragraph } from '../components'
 
 export default function Home() {
   return (
-    <main>
-      <h1>React Concepts</h1>
-      <p>
+    <Main>
+      <Heading>React Concepts</Heading>
+      <Paragraph>
         Hello, I'm covering the advanced React concepts to understand them. Most
         of these will have examples which can be played around with.
-      </p>
-      <p>
+      </Paragraph>
+      <Paragraph>
         An app made by{' '}
         <Link href='https://github.com/danielbutterfield'>Dan</Link> with
         NextJS.
-      </p>
-    </main>
+      </Paragraph>
+    </Main>
   )
 }
