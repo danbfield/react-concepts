587b32fa87a6adf5fd1581029b2b336d863162ac
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-15 19:31:13 +0100

    Index: Add introduction text

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,3 +1,18 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <div>Index</div>
+  return (
+    <main>
+      <h1>React Concepts</h1>
+      <p>
+        Hello, I'm covering the advanced React concepts to understand them. Most
+        of these will have examples which can be played around with.
+      </p>
+      <p>
+        An app made by{' '}
+        <Link href='https://github.com/danielbutterfield'>Dan</Link> with
+        NextJS.
+      </p>
+    </main>
+  )
 }
