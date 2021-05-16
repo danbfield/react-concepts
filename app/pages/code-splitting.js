37824995cb1910140bfafac277674078bee9202e
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-16 12:04:32 +0100

    Pages/CodeSplitting: Add Bundles section

--- app/pages/code-splitting.js
+++ app/pages/code-splitting.js
@@ -1,3 +1,31 @@
+import {
+  Heading,
+  // Input,
+  // Label,
+  Main,
+  Paragraph,
+  Section,
+  Subheading,
+} from '../components'
+// import Link from 'next/link'
+
 export default function CodeSplitting() {
-  return <div>Code Splitting</div>
+  return (
+    <Main>
+      <Heading>Code Splitting</Heading>
+      <Paragraph>
+        To avoid the code base getting too big, code can be split into different
+        bundles; there's a couple of ways to do this.
+      </Paragraph>
+      <Section>
+        <Subheading>Bundles</Subheading>
+        <Paragraph>
+          Bundling is the process of importing files, and merging them into a
+          single file. This bundle is then included on a web page to load the
+          entire app at once. Most React apps (such as those using
+          create-react-app) will be bundled already using Webpack.
+        </Paragraph>
+      </Section>
+    </Main>
+  )
 }
