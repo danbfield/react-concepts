f3f8d5ebfcf67b7f935071d3cf48b16b050191d0
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-17 10:44:08 +0100

    Pages/Typechecking: Add proptypes content

--- app/pages/type-checking.js
+++ app/pages/type-checking.js
@@ -1,3 +1,20 @@
+import { Heading, Main, Paragraph, Section, Subheading } from '../components'
+import Link from 'next/link'
+
 export default function TypeChecking() {
-  return <div>Type Checking</div>
+  return (
+    <Main>
+      <Heading>Type Checking</Heading>
+      <Section>
+        <Subheading>Don't have TypeScript?</Subheading>
+        <Paragraph>
+          Typechecking is a pretty common tool for catching a lot of bugs in an
+          application; the popular JavaScript superset TypeScript can typecheck
+          entire applications. In pure JavaScript, we can use{' '}
+          <Link href='https://github.com/facebook/prop-types'>PropTypes</Link>{' '}
+          to do this.
+        </Paragraph>
+      </Section>
+    </Main>
+  )
 }
