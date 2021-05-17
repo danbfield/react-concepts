4efff305c8132efc21895f4cbb8e08197472b451
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-17 18:39:55 +0100

    Pages/RenderProps: Add render props content and example

--- app/pages/render-props.js
+++ app/pages/render-props.js
@@ -1,3 +1,40 @@
+import { useEffect, useState } from 'react'
+import { Heading, Main, Paragraph, Section, Subheading } from '../components'
+import Link from 'next/link'
+
+function RenderAnything({ render }) {
+  const [data, setData] = useState({
+    text: `This a component which returns a render function. It is just a regular 
+      paragraph though, except it has state, which has this text.`,
+  })
+
+  return render(data)
+}
+
 export default function RenderProps() {
-  return <div>Render Props</div>
+  return (
+    <Main>
+      <Heading>Render Props</Heading>
+      <Section>
+        <Subheading>But which component do I render?</Subheading>
+        <Paragraph>
+          Render props is quite a neat trick we use to allow a component to
+          render different things dynamically without changing its
+          implementation. It doesn't actually need to be "render" here, it could
+          be literally name for the prop, it just makes sense to use render.
+          There's other neat tricks too, such as using this method to create
+          HOCs, or instead of using "render", use "children" which means it
+          doesn't need to be specified as a prop in the component. See{' '}
+          <Link href='https://reactjs.org/docs/render-props.html'>here</Link>{' '}
+          for more examples.
+        </Paragraph>
+        <RenderAnything
+          render={(data) => <Paragraph>{data && data.text}</Paragraph>}
+        />
+        <RenderAnything
+          render={(data) => <Subheading>{data && data.text}</Subheading>}
+        />
+      </Section>
+    </Main>
+  )
 }
