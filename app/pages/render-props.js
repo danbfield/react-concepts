14d561ab1cf1f8e113f966f42e95f883a8631f57
Daniel Butterfield <danrbutterfield@gmail.com>
2021-05-17 18:40:18 +0100

    Pages/RenderProps: Remove useEffect

--- app/pages/render-props.js
+++ app/pages/render-props.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Heading, Main, Paragraph, Section, Subheading } from '../components'
 import Link from 'next/link'
 
