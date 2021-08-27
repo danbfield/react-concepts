0915c5d5f98da3d0a5d147a610e1ec2977220a8a
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2021-08-27 02:45:54 +0100

    Add navigation toggle option

--- app/components/NavigationMenu.js
+++ app/components/NavigationMenu.js
@@ -1,16 +1,29 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
 
 export default function NavigationMenu({ children, items }) {
+  const [menuIsHidden, setMenuIsHidden] = useState(false)
+
+  const toggleMenu = () => setMenuIsHidden(!menuIsHidden)
+
+  const menuText = menuIsHidden ? 'Show 🙉' : 'Hide 🙈'
+
   return (
     <>
       <nav>
         <ul>
-          {items.map((item, index) => (
-            <li key={index}>
-              <Link href={item.url}>{item.text}</Link>
-            </li>
-          ))}
+          <li>
+            <a className="mobile__hide-menu" onClick={toggleMenu} href="#">
+              {menuText}
+            </a>
+          </li>
+          {!menuIsHidden &&
+            items.map((item, index) => (
+              <li key={index}>
+                <Link href={item.url}>{item.text}</Link>
+              </li>
+            ))}
         </ul>
       </nav>
       {children}
