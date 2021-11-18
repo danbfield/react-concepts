8d776e86603438db720169b6fb4fe4835262041d
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2021-11-18 18:18:20 +0000

    Add mobile menu click event

--- app/components/NavigationMenu.js
+++ app/components/NavigationMenu.js
@@ -2,49 +2,67 @@ import { useState } from 'react'
 import PropTypes from 'prop-types'
 import MenuIcon from './MenuIcon'
 
+const mainMenuStyle = [
+  'bg-blue-800',
+  'text-blue-100',
+  'w-1/4',
+  'space-y-6',
+  'py-8',
+  'px-4',
+  'absolute',
+  'inset-y-0',
+  'left-0',
+  'transform',
+  '-translate-x-full',
+  'transition',
+  'duration-200',
+  'ease-in-out',
+  'md:relative',
+  'md:translate-x-0',
+]
+
 export default function NavigationMenu({ items }) {
-  const [menuIsHidden, setMenuIsHidden] = useState(false)
+  const [menuClasses, setMenuClasses] = useState(mainMenuStyle)
 
-  const toggleMenu = () => setMenuIsHidden(!menuIsHidden)
+  const handleMobileButtonClick = () => {
+    const translateClass = '-translate-x-full'
 
-  const menuText = menuIsHidden ? 'Show 🙉' : 'Hide 🙈'
+    const array = menuClasses.includes(translateClass)
+      ? menuClasses.filter((element) => element !== '-translate-x-full')
+      : [...menuClasses, translateClass]
+
+    setMenuClasses(array)
+  }
 
   return (
     <>
       <div className="bg-gray-800 text-gray-100 flex justify-between md:hidden">
-        <a href="/" class="px-4 text-white flex items-center space-x-2">
+        <a href="/" className="px-4 text-white flex items-center space-x-2">
           <span className="text-2xl font-bold">React Concepts</span>
         </a>
-        <button className="p-4 focus:outline-none focus:bg-gray-700">
+        <button
+          className="p-4 focus:outline-none focus:bg-gray-700"
+          onClick={handleMobileButtonClick}
+        >
           <MenuIcon />
         </button>
       </div>
-      <div className="bg-blue-800 text-blue-100 w-1/4 space-y-6 py-8 px-4 absolute inset-y-0 left-0 transform -translate-x-full transition duration-200 ease-in-out md:relative md:translate-x-0">
-        <a href="/" class="px-4 text-white flex items-center space-x-2">
+      <div className={menuClasses.join(' ')}>
+        <a href="/" className="px-4 text-white flex items-center space-x-2">
           <span className="text-2xl font-bold">React Concepts</span>
         </a>
         <nav>
           <ul>
-            <li>
-              <a
-                className="mobile__hide-menu block py-2.5 px-4 rounded hover:bg-blue-700 transition duration-200"
-                onClick={toggleMenu}
-                href="#"
-              >
-                {menuText}
-              </a>
-            </li>
-            {!menuIsHidden &&
-              items.map((item, index) => (
-                <li key={index}>
-                  <a
-                    className="block py-2.5 px-4 rounded hover:bg-blue-700 transition duration-200"
-                    href={item.url}
-                  >
-                    {item.text}
-                  </a>
-                </li>
-              ))}
+            {items.map((item, index) => (
+              <li key={index}>
+                <a
+                  className="block py-2.5 px-4 rounded hover:bg-blue-700 transition duration-200"
+                  href={item.url}
+                >
+                  {item.text}
+                </a>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
