e3d3364c5c72863825c9ed26d453a516b984cacb
Daniel Butterfield <dan.butterfield@hotmail.co.uk>
2021-11-18 21:23:14 +0000

    Update mobile menu CSS

--- app/components/NavigationMenu.js
+++ app/components/NavigationMenu.js
@@ -3,26 +3,29 @@ import PropTypes from 'prop-types'
 import MenuIcon from './MenuIcon'
 
 const mainMenuStyle = [
-  'bg-blue-800',
-  'text-blue-100',
-  'w-1/4',
-  'space-y-6',
-  'py-8',
-  'px-4',
-  'absolute',
-  'inset-y-0',
-  'left-0',
-  'transform',
   '-translate-x-full',
-  'transition',
+  'absolute',
+  'bg-blue-800',
   'duration-200',
   'ease-in-out',
+  'inset-y-0',
+  'left-0',
   'md:relative',
   'md:translate-x-0',
+  'px-2',
+  'py-7',
+  'space-y-6',
+  'text-blue-100',
+  'transform',
+  'transition',
+  'w-64',
 ]
+
+const mobileMenuStyle = []
 
 export default function NavigationMenu({ items }) {
   const [menuClasses, setMenuClasses] = useState(mainMenuStyle)
+  const [displayingMobileMenu, setDisplayingMobileMenu] = useState(false)
 
   const handleMobileButtonClick = () => {
     const translateClass = '-translate-x-full'
@@ -32,12 +35,14 @@ export default function NavigationMenu({ items }) {
       : [...menuClasses, translateClass]
 
     setMenuClasses(array)
+    // setDisplayingMobileMenu(!displayingMobileMenu)
+    // setMenuClasses(displayingMobileMenu ? mainMenuStyle : mobileMenuStyle)
   }
 
   return (
     <>
       <div className="bg-gray-800 text-gray-100 flex justify-between md:hidden">
-        <a href="/" className="px-4 text-white flex items-center space-x-2">
+        <a href="/" className="block p-4 text-white font-bold">
           <span className="text-2xl font-bold">React Concepts</span>
         </a>
         <button
@@ -56,7 +61,7 @@ export default function NavigationMenu({ items }) {
             {items.map((item, index) => (
               <li key={index}>
                 <a
-                  className="block py-2.5 px-4 rounded hover:bg-blue-700 transition duration-200"
+                  className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white"
                   href={item.url}
                 >
                   {item.text}
