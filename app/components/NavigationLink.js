3bd7ef0a3916805fab404fdcad7dd3614f6692fa
Dan Butterfield <danrbutterfield@gmail.com>
2022-04-22 15:44:36 +0100

    Update NavigationLink.js

--- app/components/NavigationLink.js
+++ app/components/NavigationLink.js
@@ -1,14 +1,19 @@
 import Link from 'next/link'
+import PropTypes from 'prop-types'
 
-export default function NavigationLink({ text, url }, handleClick) {
-  <Link href={url}>
-    <a
-      className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white"
-      onClick={handleClick && handleClick}
-    >
-      {text}
-    </a>
+export default function NavigationLink({ item, handleClick }) {
+  const { text, url } = item
+
+  return (
+    <Link href={url}>
+      <a
+        className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white"
+        onClick={handleClick}
+      >
+        {text}
+      </a>
   </Link>
+  )
 }
 
 NavigationLink.propTypes = {
